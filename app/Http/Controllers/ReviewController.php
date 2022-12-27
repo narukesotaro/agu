5b73e4319c4aad17bf18eb7a6d89a7ce6be2994f
narukesotaro <narukesotaro11218080@icloud.com>
2022-12-27 14:27:10 +0900

    追加:Review stable&ReviewController&Review model

---
+++ app/Http/Controllers/ReviewController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Review;
+
+class ReviewController extends Controller
+{
+    public function index(Review $review)
+    {
+        return $review->get();
+    }
+}
---
+++ database/migrations/2022_12_27_110315_create_reviews_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reviews', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('about_credit');
+            $table->foreignId('user_id')->constrained();
+            $table->string('rating');
+            $table->foreignId('course_id')->constrained();  
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reviews');
+    }
+};
